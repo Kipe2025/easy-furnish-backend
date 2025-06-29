0dc73b01344862e77f73f157f8976d178335eae1
Kipe2025 <kimmo.varhala@gmail.com>
2025-06-29 14:32:31 +0300

    Add test route to auth.js for server verification

--- routes/auth.js
+++ routes/auth.js
@@ -6,6 +6,11 @@ router.get('/', (req, res) => {
   res.send('Auth route toimii!');
 });
 
+// Test route for verifying server is up
+router.get('/test', (req, res) => {
+  res.json({ message: 'Auth route is working!' });
+});
+
 // Auth middleware function
 const authMiddleware = (req, res, next) => {
   // For now, just pass through - you can add JWT verification here later
